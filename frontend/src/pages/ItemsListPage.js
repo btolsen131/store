f13ba7c2a3a5a511258925bfd5bf9b89ff0f384f
Brian Olsen <btolsen131@gmail.com>
2022-06-06 11:26:45 -0400

    moved routes location

--- frontend/src/pages/ItemsListPage.js
+++ frontend/src/pages/ItemsListPage.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react'
-
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import {Segment, Message, Loader, Dimmer, Image, Container} from 'semantic-ui-react'
---
+++ frontend/src/routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import HomepageLayout from "./components/Home";
+import './App.css';
+import ItemsListPage from './pages/ItemsListPage.js'
+import ItemPage from './pages/ItemPage'
+import About from './pages/About'
+import Home from './pages/Home'
+
+
+import Cart from './pages/Cart'
+import Login from './components/Login'
+
+const BaseRouter = () => (
+    
+    <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path='/Shop' exact element={<ItemsListPage/>}/>
+          <Route path='/Item/<:id>' element={<ItemPage />} />
+          <Route path="/About/" exact element={<About/>} />
+          <Route path="/cart/" exact element={<Cart/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/login' element={<Login/>} />
+     </Routes>
+     
+    );
+
+export default BaseRouter
