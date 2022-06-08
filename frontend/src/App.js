b9bc1e0504555f6c252058cc9e8dac166c0bf5b6
Brian Olsen <btolsen131@gmail.com>
2022-06-08 19:31:43 -0400

    redux added for product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,13 @@
-import { BrowserRouter as Router,
-  Routes,
-  Route}
-  from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 import { useState } from 'react';
 import './App.css';
-import ItemsListPage from './pages/ItemsListPage.js'
-import ItemPage from './pages/ItemPage'
 import NavBar from './components/Navbar/NavBar.js'
-import About from './pages/About'
-import Home from './pages/Home'
 import Footer from './components/Footer/Footer'
 import React from 'react'
-import Cart from './pages/Cart'
 import BaseRouter from './routes';
 
 function App() {
-  const [cart, setCart] = useState({});
-
-  // const fetchCart = async () => {
-  //   const response = await commerce.cart.retrieve()
-  // }
-
+  
   return (
     <Router>
       
--- frontend/src/pages/ItemsListPage.js
+++ frontend/src/pages/ItemsListPage.js
@@ -1,34 +1,42 @@
 import React, {useState, useEffect} from 'react'
 import {Row, Col} from 'react-bootstrap'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProducts} from '../actions/productActions'
+
 
 function ItemsListPage() {
-  const [items, setItems] = useState([])
-
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.itemList)
+  const {error, loading, items } = productList
+  
   useEffect(()=>{
+    dispatch(listProducts())
+  
+  },[dispatch])
 
-    async function fetchItems(){
-      const { data } = await axios.get('api/items/')
-      setItems(data)
-    }
-
-    fetchItems()
-  },[])
+  
 
   return (
   
         <div>
-          <Row>
-            {items.map((item) => {
-              return (
-              <Col key={item._id} smm={12} md={6} lg={4}>
-                <Product item={item} />
-              </Col>
-              )
+          {loading ? <Loader />
+            :error ? <Message variant='danger'>{error}</Message>
+            :
+              <Row>
+              {items.map((item) => {
+                return (
+                <Col key={item._id} smm={12} md={6} lg={4}>
+                  <Product item={item} />
+                </Col>
+                )
+              }
+              )}
+            </Row>
             }
-            )}
-          </Row>
+         
         </div>
 
   )}
