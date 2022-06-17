6b5261e26e461890020afd2d84a6b3534a8d2ed8
Brian Olsen <btolsen131@gmail.com>
2022-06-17 12:59:04 -0400

    added signal to overwrite username with email

--- BackEnd/storeAPI/apps.py
+++ BackEnd/storeAPI/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class StoreapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'storeAPI'
+
+
+
+    def ready(self):
+        import storeAPI.signals
