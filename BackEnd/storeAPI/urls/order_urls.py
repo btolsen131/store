22f3c56e94f9abae95f95ea6c63aad69e79a19d6
Brian Olsen <btolsen131@gmail.com>
2022-06-22 09:05:25 -0400

    backend order view added

--- BackEnd/storeAPI/urls/order_urls.py
+++ BackEnd/storeAPI/urls/order_urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     
-  
+  path('add/', views.addOrderItems, name='add-order'),
     
 ]
 
--- BackEnd/storeAPI/views/order_views.py
+++ BackEnd/storeAPI/views/order_views.py
@@ -1,15 +1,60 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from itsdangerous import Serializer
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from sqlalchemy import false
-from storeAPI.models import StoreItems
-from storeAPI.serializers import StoreItemsSerializer, UserSerializer, UserSerializerWithToken
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
+
+from storeAPI.models import StoreItems, Order, OrderItem, ShippingAddress
+from storeAPI.serializers import StoreItemsSerializer, UserSerializer, UserSerializerWithToken, OrderItemSerializer, ShippingAddressSerializer, OrderSerializer
+
 from rest_framework import status
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail':'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        #creating order
+        order = Order.objects.create(
+            user=user,
+            paymenthMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice = data['totalPrice']
+        )
+
+        #creating shipping address
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+
+        # Create order items and set order to orderItem relationship
+        for i in orderItems:
+            product = StoreItems.objects.get(_id=i['product'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+            )
+
+            # update stock in database
+
+            product.countInStock -= item.qty
+            product.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
